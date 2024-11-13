f12c9bf2321371d95f84c30ce432f326cd5023ae
Tiago Silva de Assis <tiaguiassis@gmail.com>
2024-11-13 03:51:32 -0300

    Update validateForm.js

--- src/scripts/validateForm.js
+++ src/scripts/validateForm.js
@@ -1,4 +1,4 @@
-export function myFunction() {
+export function toggleDisabledLoginBtn() {
     const btn = document.getElementById('btn-login')
     const email = document.querySelector('input#email')
     const password = document.querySelector('input#password')
@@ -9,3 +9,48 @@ export function myFunction() {
         btn.disabled = true
     }
 }
+
+export function validateEmail() {
+    const inputEmail = document.getElementById('email')
+    const isValid = /^[^\s@]+@[^\s@]+\.com/.test(inputEmail.value);
+
+    const alertMessage = document.querySelector('.login-alert-message')
+    
+    if (!isValid) {
+        alertMessage.textContent = 'E-mail inválido.'
+        return false
+    }
+    else {
+        alertMessage.textContent = ''
+        return true
+    }
+}
+
+export function restrictPasswordInput(event) {
+    const inputPassword = event.target
+    const validCharacters = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{}|\\:;"'<>,.?/~`]+$/; // Expressão regular para caracteres válidos
+
+    const alertMessage = document.querySelector('.password-alert-message');
+
+    if (!validCharacters.test(inputPassword.value)) {
+        inputPassword.value = inputPassword.value.replace(/[^a-zA-Z0-9!@#$%^&*()_+\-=[\]{}|\\:;"'<>,.?/~`]+/g, '');
+    } else {
+        alertMessage.textContent = ''
+    }
+}
+
+
+
+export function restrictEmailInput(event) {
+    const alertMessage = document.querySelector('.login-alert-message')
+    alertMessage.textContent = ''
+    const input = event.target
+    const validCharacters = /^[a-zA-Z0-9@._-]+$/
+
+    // Verifica cada caractere digitado
+    if (!validCharacters.test(input.value)) {
+        // Remove caracteres inválidos
+        input.value = input.value.replace(/[^a-zA-Z0-9@._-]+/g, '')
+    }
+}
+
